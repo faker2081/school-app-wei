bf94cbaa50574123bdc74113969bc028755d4b89
liuruixiang <2084438938@qq.com>
2023-11-20 22:16:34 +0800

    瀑布流组件

---
+++ src/pages/schoolForum/component/waterFallList/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <scroll-view scroll-y="true" class="list-box">
+    
+  </scroll-view>
+</template>
+
+<script setup>
+import {ref, reactive, watchEffect, getCurrentInstance, defineEmits } from "vue";
+
+let emits = defineEmits(['like', 'contentInfo', 'userInfo']);
+let prpos = defineProps({
+  list: {
+    type: Array,
+    default: () => []
+  }
+})
+
+
+</script>

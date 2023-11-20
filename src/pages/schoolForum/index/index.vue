7b2f249c52fcf5328d2dc4edaeae5387fdb6de8a
liuruixiang <2084438938@qq.com>
2023-11-20 22:15:36 +0800

    首页

--- src/pages/schoolForum/index/index.vue
+++ src/pages/schoolForum/index/index.vue
@@ -1,54 +1,24 @@
 <template>
-  <view class="header-1">
-    <uv-tabs
-      :list="list"
-      lineWidth="0"
-      lineColor="#f56c6c"
-      :activeStyle="{
-        color: '#EC808D',
-        fontWeight: 'bold',
-        transform: 'scale(1.05)',
-      }"
-      :inactiveStyle="{
-        color: '#606266',
-        transform: 'scale(1)',
-      }"
-      itemStyle="padding-left: 15px; padding-right: 15px; height: 34px;"
-      @change="changeTab"
-    ></uv-tabs>
-    <view class="header-1__right" style="display: flex;">
-      <text>同城</text> <uv-icon name="arrow-down" style="">23</uv-icon>
+  <view class="box-all">
+    <view class="header-box">
+      <tabsBar-1></tabsBar-1>
+      <tabsBar-2></tabsBar-2>
+    </view>
+    <view class="content-box">
+      <waterFallList></waterFallList>
     </view>
   </view>
 </template>
 
 <script setup>
+import tabsBar1 from '../component/tabs/tabsBar-1/index';
+import tabsBar2 from '../component/tabs/tabsBar-2/index';
+import waterFallList from '../component/waterFallList/index';
 import { ref } from 'vue'
-const list = ref([
-  { name: '关注',  code: '1', },
-  { name: '发现', code: '2', },
-  { name: '同城', code: '3', }
-])
 
-// 切换标签
-const changeTab = (item) => {
-  console.log(item)
-}
 </script>
-
-<style lang="scss">
-.header-1 {
-  height: 44px;
-  background-color: #fff;
-  border-bottom: 1px solid #f0f0f0;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding-left: 15px;
-  padding-right: 15px;
-  .header-1__right{
-    display: flex;
-    
-  }
+<style scoped lang="scss">
+.box-all {
+  overflow: hidden;
 }
 </style>

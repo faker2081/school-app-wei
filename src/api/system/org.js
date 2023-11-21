65ae31d725cf1fb17350744ebff8d31c90ce1fda
liuruixiang <2084438938@qq.com>
2023-11-21 22:41:58 +0800

    删除不必要文件

--- src/api/system/org.js
+++
@@ -1,8 +0,0 @@
-import baseUrl from '@/api/env.js'
-export default {
-    // 查询档口基本信息列表,支持翻页
-    orgPage: baseUrl + '/system/org/page',
-    // 导出档口基本信息列表
-    orgList: baseUrl + '/system/org/list',
-
-}
